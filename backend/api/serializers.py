74599247b39b5265bd4bf22cb4df23368b225b50
“ariel.duarte” <arielpy@gmail.com>
2024-09-07 13:04:23 -0300

    fix article serializers to create an article

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -7,42 +7,34 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["id", "username", "password"]
-        extra_kwargs = {"password": {"write_only": True}} # password is not return de password to the user in the response
+        # password is not return de password to the user in the response
+        extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
         print(validated_data)
         user = User.objects.create_user(**validated_data)
         return user
-    
+
+
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ["id", "name", "description"]
-    
 
     def create(self, validated_data):
         category = Category.objects.create(**validated_data)
         return category
 
+
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
-        fields = ["id", "title", "content", "publication_date", "author", "category"]
-        extra_kwargs = {"author": {"read_only": True}}  # author is not return de author to the user in the response
+        fields = ["id", "title", "content",
+                  "publication_date", "author", "category"]
+        # author is not return de author to the user in the response
+        extra_kwargs = {"author": {"read_only": True}}
 
     # Si quieres permitir crear o actualizar artículos con una categoría existente
     def create(self, validated_data):
-        category_data = validated_data.pop('category')
-        category = Category.objects.get(**category_data)
-        article = Article.objects.create(category=category **validated_data)
+        article = Article.objects.create(**validated_data)
         return article
-
-    def update(self, instance, validated_data):
-        category_data = validated_data.pop('category')
-        category = Category.objects.get(**category_data)
-        instance.category = category
-        instance.title = validated_data.get('title', instance.title)
-        instance.content = validated_data.get('content', instance.content)
-        instance.publication_date = validated_data.get('publication_date', instance.publication_date)
-        instance.save()
-        return instance
