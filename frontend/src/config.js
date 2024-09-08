5e542837b7a2f3098b51782fe39a4616e1039961
ariel.duarte <arielpy@gmail.com>
2024-09-08 02:36:06 -0300

    Added choreo api url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,1 +1,3 @@
-export const API_URL = process.env.REACT_APP_API_BASE_URL;
+const apiUrl = "/choreo-apis/infoweb/backend/v1";
+const localApiUrl = process.env.REACT_APP_API_BASE_URL;
+export const API_URL = localApiUrl ? localApiUrl : apiUrl;
